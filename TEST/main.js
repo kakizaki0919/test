e3e04d440642363b51469bebf245fa96f567b3d6
kakizaki0919 <91304715+kakizaki0919@users.noreply.github.com>
2023-10-25 10:58:48 +0900

    Add files via upload

--- TEST/main.js
+++ TEST/main.js
@@ -2,32 +2,39 @@
 let lat = document.getElementById('lat');; // 緯度
 let lng = document.getElementById('lng'); // 経度
 let zoom = 16; // ズームレベル
-navigator.geolocation.getCurrentPosition((position)=>{   
-  let coords = position.coords;
+var n = 0;
+let map = L.map("map"); // 地図の生成
+// タイルレイヤを生成し、地図に追加する
+    // 今回はOpenStreetMapを表示する
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+      {
+        // 著作物の表示
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }
+    ).addTo(map);
+function getPos(){
+  navigator.geolocation.getCurrentPosition((position)=>{   
+    let coords = position.coords;
+    
+    lat.textContent = coords.latitude;
+    lng.textContent = coords.longitude;
   
-  lat.textContent = coords.latitude;
-  lng.textContent = coords.longitude;
-  
-  let lat2 = coords.latitude.toFixed(2)
-  let lng2 = coords.longitude.toFixed(2)
 
 
-  console.log(lat.textContent);
+    console.log(lat.textContent);
+    console.log(lng.textContent);
 
-  let map = L.map("map"); // 地図の生成
-  map.setView([lat.textContent,lng.textContent], zoom); // 緯度経度、ズームレベルを設定する
+    
+    map.setView([lat.textContent,lng.textContent], zoom); // 緯度経度、ズームレベルを設定する
 
-  // タイルレイヤを生成し、地図に追加する
-  // 今回はOpenStreetMapを表示する
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-    {
-      // 著作物の表示
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }
-  ).addTo(map);
+    
 
-  const mark = L.marker([lat.textContent,lng.textContent]);
-  mark.addTo(map);
+    const mark = L.marker([lat.textContent,lng.textContent]);
+    mark.addTo(map);
+    n++;
+    console.log(n);
 
-});
+  });
+}
+setInterval("getPos()",5000);
 
